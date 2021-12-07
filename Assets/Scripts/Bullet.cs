3908fd56b15a24760d83012afcb4d3ecf0d40231
unBonChasseur <54935237+unBonChasseur@users.noreply.github.com>
2021-12-07 15:33:10 +0100

    update bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -9,23 +9,25 @@ public class Bullet : MonoBehaviour
 
     [SerializeField]
     private Camera m_mainCamera;
-    private Vector3 m_initialPosition;
     private Vector3 m_currentPosition;
+
+    [SerializeField]
+    private float m_deathDist;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_initialPosition = m_mainCamera.WorldToScreenPoint(transform.position);
+
     }
 
     // Update is called once per frame
     void Update()
     {
         m_currentPosition = m_mainCamera.WorldToScreenPoint(transform.position);
-        Debug.Log(m_currentPosition + "||" + m_initialPosition);
+
         transform.position += new Vector3(0, m_movementSpeed * Time.deltaTime, 0);
 
-        if (m_currentPosition.y > 450.0f)
-            Destroy(this, 0);
+        if (m_currentPosition.y > m_deathDist)
+            Destroy(gameObject, 0);
     }
 }
