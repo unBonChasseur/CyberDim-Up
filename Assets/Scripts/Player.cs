7a3acc085ccabcd76fa99b20b4f9325a7b414118
unBonChasseur <54935237+unBonChasseur@users.noreply.github.com>
2021-12-09 16:49:53 +0100

    Deplacement + Rotation OK
    Next step :
    - Resolve Game screen bug
    - Bullet calibration

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,18 +8,27 @@ public class Player : Entity
     [Header("Level")]
     [SerializeField]
     private int m_LevelNumber;
-    private int m_LevelNumberTotal = 3;
 
-    [Header("Gameplay")]
+    [Header("Movements")]
     [SerializeField]
     private float m_movementSpeed;
+    [SerializeField]
+    private float m_smooth;
+    [SerializeField]
+    private float m_tiltAngle;
 
     [Header("Camera")]
     [SerializeField]
+    private GameObject[] m_Cinemachines;
+    private int m_NbCinemachines;
+    private Stopwatch m_CamStopWatch;
+
+    [SerializeField]
     private Camera m_mainCamera;
     [SerializeField]
-    private GameObject[] m_numCamera;
-
+    private float m_marginCamera;
+    [SerializeField]
+    private float m_camTransitionTime;
     private Vector3 m_initialPosition;
     private Vector3 m_currentPosition;
 
@@ -28,126 +37,148 @@ public class Player : Entity
     private float m_fireRateDelay;
     [SerializeField]
     private GameObject m_PrefabFire;
-    private Stopwatch m_StopWatch;
+    private Stopwatch m_FireStopWatch;
 
-    public bool defaite=false;
 
-    private float score = 0;
+    public bool m_defaite;
+
+    private float m_score;
 
     void Awake()
     {
-        m_StopWatch = new Stopwatch();
-        m_StopWatch.Start();
-        
+        m_CamStopWatch = new Stopwatch();
+        m_CamStopWatch.Start();
+
+        m_FireStopWatch = new Stopwatch();
+        m_FireStopWatch.Start();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        m_NbCinemachines = m_Cinemachines.Length;
+        m_camTransitionTime = 2000;
+        m_defaite = false;
+        m_score = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         m_currentPosition = m_mainCamera.WorldToScreenPoint(transform.position);
-
-        if (!m_numCamera[m_LevelNumber % m_LevelNumberTotal].activeSelf)
+        //UnityEngine.Debug.Log("x : " + m_currentPosition.x + " < " + (m_initialPosition.x - m_marginCamera));
+        //UnityEngine.Debug.Log("y : " + m_currentPosition.y + " < " + (m_initialPosition.y * 2 - m_marginCamera));
+        
+        // Changement de caméra en fonction du niveau sélectionné
+        if (!m_Cinemachines[m_LevelNumber % m_NbCinemachines].activeSelf)
         {
-            m_numCamera[m_LevelNumber % m_LevelNumberTotal].SetActive(true);
-            m_numCamera[(m_LevelNumber + 1) % m_LevelNumberTotal].SetActive(false);
-            m_numCamera[(m_LevelNumber + 2) % m_LevelNumberTotal].SetActive(false);
+            for (int i = 0; i < m_NbCinemachines; i++)
+                m_Cinemachines[i].SetActive(false);
 
-            m_initialPosition = m_mainCamera.WorldToScreenPoint(transform.position);
+            transform.position = new Vector3(0, -1000, -1000);
+            m_Cinemachines[m_LevelNumber % m_NbCinemachines].SetActive(true);
+
+            m_CamStopWatch.Restart();
         }
 
-        if (m_LevelNumber % m_LevelNumberTotal == 0)
+        if(m_CamStopWatch.ElapsedMilliseconds >= m_camTransitionTime)
         {
-
-            if (Input.GetKey(KeyCode.UpArrow) && m_currentPosition.y < m_initialPosition.y * 2)
-                transform.position += new Vector3(0, m_movementSpeed * Time.deltaTime, 0);
-
-            if (Input.GetKey(KeyCode.DownArrow) && m_currentPosition.y > 0)
-                transform.position -= new Vector3(0, m_movementSpeed * Time.deltaTime, 0);
-
-            if (Input.GetKey(KeyCode.Space))
+            if (m_LevelNumber % m_NbCinemachines == 1)
             {
-                if (m_StopWatch.ElapsedMilliseconds >= m_fireRateDelay)
+
+                // Gestion des déplacements
+                if (Input.GetKey(KeyCode.UpArrow) && m_currentPosition.y < (m_initialPosition.y * 2) - m_marginCamera)
+                    transform.position += new Vector3(0, m_movementSpeed * Time.deltaTime, 0);
+
+                if (Input.GetKey(KeyCode.DownArrow) && m_currentPosition.y > m_marginCamera)
+                    transform.position -= new Vector3(0, m_movementSpeed * Time.deltaTime, 0);
+
+                // Gestion des tirs
+                if (Input.GetKey(KeyCode.Space))
                 {
-                    GameObject FireLeft = Instantiate(m_PrefabFire, transform.position + new Vector3(-0.305f, 0.08f, -3.6f), new Quaternion(0, 90, 90, 1));
-                    GameObject FireRight = Instantiate(m_PrefabFire, transform.position + new Vector3(0.305f, 0.08f, -3.6f), new Quaternion(0, 90, 90, 1));
-                    Bullet.OnHit += OnBulletHit;
-                    m_StopWatch.Restart();
+                    if (m_FireStopWatch.ElapsedMilliseconds >= m_fireRateDelay)
+                    {
+                        // On créé les tirs pour qu'ils partent exactement des canons
+                        GameObject FireLeft = Instantiate(m_PrefabFire, transform.position + new Vector3(-0.305f, 0.08f, -3.6f), new Quaternion(0, 90, 90, 1));
+                        GameObject FireRight = Instantiate(m_PrefabFire, transform.position + new Vector3(0.305f, 0.08f, -3.6f), new Quaternion(0, 90, 90, 1));
+                        Bullet.OnHit += OnBulletHit;
+                        m_FireStopWatch.Restart();
+                    }
                 }
             }
-        }
-        else if (m_LevelNumber % m_LevelNumberTotal == 1)
-        {
-
-            if (Input.GetKey(KeyCode.RightArrow))
-                transform.position -= new Vector3(m_movementSpeed * Time.deltaTime, 0, 0);
-
-            if (Input.GetKey(KeyCode.LeftArrow))
-                transform.position += new Vector3(m_movementSpeed * Time.deltaTime, 0, 0);
-
-            if (Input.GetKey(KeyCode.UpArrow))
-                transform.position -= new Vector3(0, 0, m_movementSpeed * Time.deltaTime);
-
-            if (Input.GetKey(KeyCode.DownArrow))
-                transform.position += new Vector3(0, 0, m_movementSpeed * Time.deltaTime);
-
-            if (Input.GetKey(KeyCode.Space))
+            else if (m_LevelNumber % m_NbCinemachines == 2)
             {
-                if (m_StopWatch.ElapsedMilliseconds >= m_fireRateDelay)
+                // Gestion des déplacements
+                if (Input.GetKey(KeyCode.LeftArrow) && m_currentPosition.x > m_marginCamera)
+                    transform.position += new Vector3(m_movementSpeed * Time.deltaTime, 0, 0);
+
+                if (Input.GetKey(KeyCode.RightArrow) && m_currentPosition.x < (m_initialPosition.x*2) - m_marginCamera)
+                    transform.position -= new Vector3(m_movementSpeed * Time.deltaTime, 0, 0);
+
+                if (Input.GetKey(KeyCode.DownArrow) && m_currentPosition.y > m_marginCamera)
+                    transform.position += new Vector3(0, 0, m_movementSpeed * Time.deltaTime);
+
+                if (Input.GetKey(KeyCode.UpArrow) && m_currentPosition.y < (m_initialPosition.y * 2) - m_marginCamera)
+                    transform.position -= new Vector3(0, 0, m_movementSpeed * Time.deltaTime);
+
+                // Gestion des tirs
+                if (Input.GetKey(KeyCode.Space))
                 {
-                    GameObject FireLeft = Instantiate(m_PrefabFire, transform.position + new Vector3(-0.305f, 0.08f, -3.6f), new Quaternion(0, 90, 90, 1));
-                    GameObject FireRight = Instantiate(m_PrefabFire, transform.position + new Vector3(0.305f, 0.08f, -3.6f), new Quaternion(0, 90, 90, 1));
-                    Bullet.OnHit += OnBulletHit;
-                    m_StopWatch.Restart();
+                    if (m_FireStopWatch.ElapsedMilliseconds >= m_fireRateDelay)
+                    {
+                        // On créé les tirs pour qu'ils partent exactement des canons
+                        GameObject FireLeft = Instantiate(m_PrefabFire, transform.position + new Vector3(-0.305f, 0.08f, -3.6f), new Quaternion(0, 90, 90, 1));
+                        GameObject FireRight = Instantiate(m_PrefabFire, transform.position + new Vector3(0.305f, 0.08f, -3.6f), new Quaternion(0, 90, 90, 1));
+                        Bullet.OnHit += OnBulletHit;
+                        m_FireStopWatch.Restart();
+                    }
+                }
+            }
+            else if (m_LevelNumber % m_NbCinemachines == 3)
+            {
+                // Gestion des Rotations
+                float tiltAroundZ = Input.GetAxis("Horizontal") * -m_tiltAngle;
+                float tiltAroundX = Input.GetAxis("Vertical") * -m_tiltAngle;
+
+                Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+
+                transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * m_smooth);
+
+                // Gestion des déplacements
+                if (Input.GetKey(KeyCode.LeftArrow) && m_currentPosition.x > m_marginCamera)
+                    transform.position += new Vector3(m_movementSpeed * Time.deltaTime, 0, 0);
+
+                if (Input.GetKey(KeyCode.RightArrow) && m_currentPosition.x < (m_initialPosition.x*2) - m_marginCamera)
+                    transform.position -= new Vector3(m_movementSpeed * Time.deltaTime, 0, 0);
+
+                if (Input.GetKey(KeyCode.UpArrow) && m_currentPosition.y < (m_initialPosition.y * 2) - m_marginCamera)
+                    transform.position += new Vector3(0, m_movementSpeed * Time.deltaTime, 0);
+
+                if (Input.GetKey(KeyCode.DownArrow) && m_currentPosition.y > m_marginCamera)
+                    transform.position -= new Vector3(0, m_movementSpeed * Time.deltaTime, 0);
+
+                // Gestion des tirs
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    if (m_FireStopWatch.ElapsedMilliseconds >= m_fireRateDelay)
+                    {
+                        // On créé les tirs pour qu'ils partent exactement des canons
+                        GameObject FireLeft = Instantiate(m_PrefabFire, transform.position + new Vector3(-0.305f, 0.08f, -3.6f), new Quaternion(0, 90, 90, 1));
+                        GameObject FireRight = Instantiate(m_PrefabFire, transform.position + new Vector3(0.305f, 0.08f, -3.6f), new Quaternion(0, 90, 90, 1));
+                        Bullet.OnHit += OnBulletHit;
+                        m_FireStopWatch.Restart();
+                    }
                 }
             }
         }
         else
         {
-
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                transform.position += new Vector3(m_movementSpeed * Time.deltaTime, 0, 0);
-            }
-
-            if (Input.GetKey(KeyCode.LeftArrow))
-            {
-                transform.position -= new Vector3(m_movementSpeed * Time.deltaTime, 0, 0);
-            }
-
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                transform.position += new Vector3(0, m_movementSpeed * Time.deltaTime, 0);
-            }
-
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                transform.position -= new Vector3(0, m_movementSpeed * Time.deltaTime, 0);
-            }
-
-            if (Input.GetKey(KeyCode.Space))
-            {
-                if (m_StopWatch.ElapsedMilliseconds >= m_fireRateDelay)
-                {
-                    GameObject FireLeft = Instantiate(m_PrefabFire, transform.position + new Vector3(-0.305f, 0.08f, -3.6f), new Quaternion(0, 90, 90, 1));
-                    GameObject FireRight = Instantiate(m_PrefabFire, transform.position + new Vector3(0.305f, 0.08f, -3.6f), new Quaternion(0, 90, 90, 1));
-                    Bullet.OnHit += OnBulletHit;
-                    m_StopWatch.Restart();
-                }
-            }
+            m_initialPosition = m_mainCamera.WorldToScreenPoint(transform.position);
+            //UnityEngine.Debug.Log(m_initialPosition + "<- Init || Current ->" + m_currentPosition);
         }
     }
 
 
-    private void InitializeCamera()
-    {
-        m_initialPosition = m_mainCamera.WorldToScreenPoint(transform.position);
-    }
     void OnCollisionEnter(Collision collision)
     {
         float hp_max = current_hp;
@@ -163,7 +194,7 @@ public class Player : Entity
                 else
                 {
                     Destroy(gameObject);
-                    defaite = true;
+                    m_defaite = true;
                 }
 
 
@@ -182,7 +213,7 @@ public class Player : Entity
                 else
                 {
                     Destroy(gameObject);
-                    defaite = true;
+                    m_defaite = true;
                 }
 
             }
@@ -190,7 +221,7 @@ public class Player : Entity
     }
     void OnBulletHit()
     {
-        score += 1;
+        m_score += 1;
     }
 
 
